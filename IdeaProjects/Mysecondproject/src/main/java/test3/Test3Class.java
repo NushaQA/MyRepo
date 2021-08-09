2b2d6a612436158c41a7334dac4307c751240070
NushaQA <nusha.shch@gamail.com>
2021-08-09 22:12:07 +0300

    Commit 08/09*2

--- IdeaProjects/Mysecondproject/src/main/java/test3/Test3Class.java
+++ IdeaProjects/Mysecondproject/src/main/java/test3/Test3Class.java
@@ -24,14 +24,11 @@ public class Test3Class {
         if (sc.hasNextInt()) {
             int number = sc.nextInt();
             int res = 1;
-            if (number < 0) {
-                System.out.println("Извините, Вы ввели не натуральное число и у него нет факториала. Перезапустите программу и попробуйте снова!");
-            } else if (number == 0) {
-                System.out.println("Спасибо! Вы ввели число " + number);
-                System.out.println("Факториал Вашего числа равен " + res);
+            if (number <= 0) {
+                System.out.println("Извините, Вы ввели не натуральное число. Перезапустите программу и попробуйте снова!");
             } else {
                 for (int i = 1; i <= number; i++) {
-                    res = res*i;
+                    res = res * i;
                 }
                 System.out.println("Спасибо! Вы ввели число " + number);
                 System.out.println("Факториал Вашего числа равен " + res);
@@ -40,14 +37,28 @@ public class Test3Class {
             System.out.println("Извините, но это явно не число. Перезапустите программу и попробуйте снова!");
         }
 
+        /*Task3: Выведите на экран все положительные делители натурального числа,
+        введённого пользователем с клавиатуры*/
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("Введите число:");
 
-
-        /*int a = 1;
-        do {
-            System.out.println(a);
-            a++;
-        } while (a<=1); */
-
+        if (sc1.hasNextInt()) {
+            int number1 = sc1.nextInt();
+            if (number1 <= 0) {
+                System.out.println("Извините, Вы ввели не натуральное число. Перезапустите программу и попробуйте снова!");
+            } else {
+                System.out.print("Число " + number1 + " делится на : ");
+                for (int j = 1; j <= number1; j++) {
+                    if ((number1 % j) == 0) {
+                        System.out.print(j + "; ");
+                    }
+                }
+            }
+        } else {
+            System.out.println("Извините, но это явно не число. Перезапустите программу и попробуйте снова!");
+        }
 
     }
 }
+
+
