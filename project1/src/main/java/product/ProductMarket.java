b5fc736a7cf84508a945a313f8640f18b9ce9555
NushaQA <nusha.shch@gmail.com>
2021-10-30 00:19:25 +0300

    Commit project1#methods

---
+++ project1/src/main/java/product/ProductMarket.java
@@ -0,0 +1,60 @@
+package product;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ProductMarket {
+
+    List<Product> products;
+
+    public ProductMarket(List<Product> products) {
+        this.products = products;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public List<Integer> getAllPrice() {
+        return products.stream()
+                .map(Product::getPrice)
+                .collect(Collectors.toList());
+    }
+
+    public List<Integer> getAllPricePositive() {
+        return products.stream()
+                .filter(x->x.getPrice()>0)
+                .map(Product::getPrice)
+                .collect(Collectors.toList());
+    }
+
+    public List<Integer> getAllPriceNegative() {
+        return products.stream()
+                .filter(x->x.getPrice()<0)
+                .map(Product::getPrice)
+                .collect(Collectors.toList());
+    }
+
+    public List<String> getAllPriceAsString() {
+        return products.stream()
+                .map(Product::getPrice)
+                .map(Objects::toString)
+                .collect(Collectors.toList());
+    }
+
+    public List<String> getAllName() {
+        return products.stream()
+                .map(Product::getName)
+                .collect(Collectors.toList());
+    }
+
+    public List<String> getAllNameSorted() {
+        return products.stream()
+                .sorted(Comparator.comparing(Product::getName))
+                .map(Product::getName)
+                .collect(Collectors.toList());
+    }
+
+
+}
